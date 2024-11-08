4bb1abf403d863751b0f9475c7d78b32a86f387d
Hooreen Fathima <hooreenfathima9@gmail.com>
2024-11-08 14:34:46 +0530

    Add files via upload

---
+++ qns.js
@@ -0,0 +1,10 @@
+let yes=document.getElementById('yes');
+let no=document.getElementById('no');
+
+yes.addEventListener('click',()=>{
+    alert("THANK YOU")
+});
+
+no.addEventListener('click',()=>{
+    alert("GO TO HELL")
+});
